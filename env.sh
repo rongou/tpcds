4473ebe5ba36e79af525ce683f318746c0577176
Rong Ou <rong.ou@gmail.com>
2020-08-14 16:21:48 -0700

    separate out nvtx jars

--- env.sh
+++ env.sh
@@ -6,6 +6,8 @@ export JAR_HOME=/opt/rapids
 
 export SPARK_CUDF_JAR=${JAR_HOME}/cudf.jar
 export SPARK_CUDF_PTDS_JAR=${JAR_HOME}/cudf-ptds.jar
+export SPARK_CUDF_NVTX_JAR=${JAR_HOME}/cudf-nvtx.jar
+export SPARK_CUDF_NVTX_PTDS_JAR=${JAR_HOME}/cudf-nvtx-ptds.jar
 export SPARK_RAPIDS_PLUGIN_JAR=${JAR_HOME}/rapids-4-spark.jar
 export SPARK_RAPIDS_INTEGRATION_TESTS_JAR=${JAR_HOME}/rapids-4-spark-integration-tests.jar
 

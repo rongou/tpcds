098df6a1855fb3e4136172e7c75ed0a228559ab5
Rong Ou <rong.ou@gmail.com>
2020-11-05 11:16:46 -0800

    flexible configs

--- benchmark-all.sh
+++ benchmark-all.sh
@@ -5,13 +5,25 @@ set -e
 DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${DIR}" ]]; then DIR="${PWD}"; fi
 
-queries=(q{1..13} q14{a,b} q{15..22} q{23,24}{a,b} q{25..38} q39{a,b} q{40..99} ss_max{,b})
-skip=(q4 q11 q13 q14a q14b q16 q17 q20 q23a q23b q24a q24b q25 q29 q50 q64 q67 q72 q95)
+queries=(q{1..13} q14{a,b} q{15..22} q{23,24}{a,b} q{25..38} q39{a,b} q{40..71} q{73..99} ss_max{,b})
+
+declare -A configs
+for i in "${queries[@]}"; do
+  configs[${i}]="6 6 1g 1g"
+done
+
+configs[q4]="6 24 1g 1g"
+configs[q11]="6 24 1g 1g"
+configs[q14a]="6 12 1g 1g"
+configs[q14b]="6 12 1g 1g"
+configs[q64]="5 5 1g 1g"
+configs[q74]="6 18 1g 1g"
+configs[q78]="6 12 1g 1g"
+configs[q95]="6 30 1g 1g"
 
 for i in "${queries[@]}"; do
-  if [[ ! "${skip[*]}" =~ ${i} ]]; then
-    "${DIR}"/benchmark.sh "${i}" 4 64 512m 2g l
-    "${DIR}"/benchmark.sh "${i}" 4 64 512m 2g p
-    rm ./*.json
-  fi
+  config="${configs[${i}]}"
+  "${DIR}"/benchmark.sh "${i}" ${config} l
+  "${DIR}"/benchmark.sh "${i}" ${config} p
+  rm ./*.json
 done
--- benchmark.sh
+++ benchmark.sh
@@ -6,11 +6,11 @@ if [[ ! -d "${DIR}" ]]; then DIR="${PWD}"; fi
 # shellcheck source=.
 source "${DIR}"/setup.sh
 
-echo "dropping caches"
-sudo sysctl vm.drop_caches=3
+#echo "dropping caches"
+#sudo sysctl vm.drop_caches=3
 
-"${DIR}"/stop.sh
-"${DIR}"/start.sh
+#"${DIR}"/stop.sh
+#"${DIR}"/start.sh
 "${DIR}"/spark-shell-gpu.sh -i <(
   echo "val args = Array(\"${DATA_DIR}\", \"${QUERY}\")"
   cat "${DIR}"/query.scala

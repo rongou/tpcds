2115699f8e0a2e1cbdd31dc4cdc37cb0ebee2a4e
Rong Ou <rong.ou@gmail.com>
2021-01-14 11:20:42 -0800

    move resource specs into env.sh

--- env.sh
+++ env.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+export SPARK_BASE_DIR=/opt
+
 # Spark
-export SPARK_HOME=/opt/spark
-export JAR_HOME=/opt/rapids
+export SPARK_HOME=${SPARK_BASE_DIR}/spark
+export JAR_HOME=${SPARK_BASE_DIR}/rapids
 
 export SPARK_CUDF_JAR=${JAR_HOME}/cudf.jar
 export SPARK_CUDF_NVTX_JAR=${JAR_HOME}/cudf-nvtx.jar
@@ -12,13 +14,16 @@ export SPARK_RAPIDS_INTEGRATION_TESTS_JAR=${JAR_HOME}/rapids-4-spark-integration
 export SPARK_MASTER_HOST=127.0.0.1
 export SPARK_LOCAL_IP=${SPARK_MASTER_HOST}
 
+export SPARK_EXECUTOR_INSTANCES=1
+export SPARK_EXECUTOR_CORES=6
+export SPARK_TASK_RESOURCE_GPU_AMOUNT=0.1666
+
 # Hadoop
-export HADOOP_HOME=/opt/hadoop
+export HADOOP_HOME=${SPARK_BASE_DIR}/hadoop
 export LD_LIBRARY_PATH=${HADOOP_HOME}/lib/native${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
 # Profiling
 export NVTX_ENABLED=false
 
 # Data
-export DATA_DIR="/opt/data/tpcds-100GB"
-#export DATA_DIR="/opt/data/tpcds-1TB"
+export DATA_DIR="${SPARK_BASE_DIR}/data/tpcds-100GB"
--- spark-shell-gpu.sh
+++ spark-shell-gpu.sh
@@ -33,13 +33,13 @@
 \
  --conf spark.executor.extraClassPath="${SPARK_CUDF_JAR}":"${SPARK_RAPIDS_PLUGIN_JAR}"\
  --conf spark.executor.extraJavaOptions=-Dai.rapids.cudf.nvtx.enabled="${NVTX_ENABLED}"\
- --num-executors 1\
- --conf spark.executor.cores=6\
+ --conf spark.executor.instances="${SPARK_EXECUTOR_INSTANCES}"\
+ --conf spark.executor.cores="${SPARK_EXECUTOR_CORES}"\
  --conf spark.executor.memory=64G\
  --conf spark.executor.resource.gpu.amount=1\
 \
  --conf spark.task.cpus=1\
- --conf spark.task.resource.gpu.amount=0.1666\
+ --conf spark.task.resource.gpu.amount="${SPARK_TASK_RESOURCE_GPU_AMOUNT}"\
 \
  --jars "${SPARK_CUDF_JAR}","${SPARK_RAPIDS_PLUGIN_JAR}","${SPARK_RAPIDS_INTEGRATION_TESTS_JAR}"\
  "$@"

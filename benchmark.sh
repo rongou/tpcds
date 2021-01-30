f72b111b5a45b8c4532a546f072ba86e642a594c
Rong Ou <rong.ou@gmail.com>
2021-01-29 20:39:39 -0800

    check spilled buffers

--- benchmark.sh
+++ benchmark.sh
@@ -10,12 +10,15 @@ source "${DIR}"/setup.sh
 #vmtouch -t -m 20G "${DATA_DIR}"
 
 # evict page cache
-#vmtouch -e "${DATA_DIR}"
+vmtouch -e "${DATA_DIR}"
 
 # clean up work directory
-#rm -fr "${SPARK_HOME}"/work/*
+rm -fr "${SPARK_HOME}"/work/*
 
 "${DIR}"/spark-shell-gpu.sh -i <(
   echo "val args = Array(\"${DATA_DIR}\", \"${QUERY}\")"
   cat "${DIR}"/query.scala
 )
+
+echo "Checking spilled buffers:"
+egrep Spilled "${SPARK_HOME}"/work/*/*/stderr"" | wc -l

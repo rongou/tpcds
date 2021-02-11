9c0cc1b7cd821cdc31868bff9332b3fe646a9027
Rong Ou <rong.ou@gmail.com>
2021-02-10 16:39:59 -0800

    include q72

--- benchmark-all.sh
+++ benchmark-all.sh
@@ -5,8 +5,8 @@ set -e
 DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${DIR}" ]]; then DIR="${PWD}"; fi
 
-queries=(q{1..13} q14{a,b} q{15..22} q{23,24}{a,b} q{25..38} q39{a,b} q{40..71} q{73..99} ss_max{,b})
-failed_queries=(q10 q31 q35 q47 q57 q59 q64 q69)
+queries=(q{1..13} q14{a,b} q{15..22} q{23,24}{a,b} q{25..38} q39{a,b} q{40..99} ss_max{,b})
+failed_queries=(q10 q31 q35 q47 q57 q59 q64 q69 q72)
 
 declare -A configs
 for i in "${queries[@]}"; do

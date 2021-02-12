6d82880f0b16b1eb9a324b3bb6f145cb62f7f420
Rong Ou <rong.ou@gmail.com>
2021-02-12 14:42:29 -0800

    separate benchmark all scripts

---
+++ benchmark-dell.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${DIR}" ]]; then DIR="${PWD}"; fi
+
+queries=(q{1..13} q14{a,b} q{15..22} q{23,24}{a,b} q{25..38} q39{a,b} q{40..99} ss_max{,b})
+failed_queries=()
+
+declare -A configs
+for i in "${queries[@]}"; do
+  configs[${i}]="3 200 1g 1g"
+done
+
+for i in "${queries[@]}"; do
+  if [[ "${failed_queries[*]}" =~ ${i} ]]; then
+    continue
+  fi
+  config="${configs[${i}]}"
+  "${DIR}"/benchmark.sh "${i}" "${config}" b
+  "${DIR}"/benchmark.sh "${i}" "${config}" g
+  rm ./*.json
+done

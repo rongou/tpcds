2720f01eeac2d9b758a2e7af224be21dfcdb62ce
Rong Ou <rong.ou@gmail.com>
2021-03-23 14:37:26 -0700

    move to spark 3.1.1 again

--- benchmark-dell.sh
+++ benchmark-dell.sh
@@ -5,7 +5,12 @@ set -e
 DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${DIR}" ]]; then DIR="${PWD}"; fi
 
-queries=(q{1..13} q14{a,b} q{15..22} q{23,24}{a,b} q{25..38} q39{a,b} q{40..99} ss_max{,b})
+# All queries:
+#queries=(q{1..13} q14{a,b} q{15..22} q{23,24}{a,b} q{25..38} q39{a,b} q{40..99} ss_max{,b})
+
+# Spilling queries:
+queries=(q4 q11 q13 q14a q14b q16 q17 q23a q23b q24a q24b q25 q29 q64 q78 q80 q93 q95)
+
 failed_queries=(q14a q14b q72)
 
 declare -A configs
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 "${SPARK_HOME}"/sbin/start-master.sh
-"${SPARK_HOME}"/sbin/start-slave.sh spark://"${SPARK_MASTER_HOST}":7077
+"${SPARK_HOME}"/sbin/start-worker.sh spark://"${SPARK_MASTER_HOST}":7077

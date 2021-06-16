b3787408494194002a38b8ce6363e1259b5c82b9
Rong Ou <rong.ou@gmail.com>
2021-06-16 14:45:21 -0700

    update dgx2

--- benchmark-dgx2.sh
+++ benchmark-dgx2.sh
@@ -6,19 +6,12 @@ DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${DIR}" ]]; then DIR="${PWD}"; fi
 
 queries=(q{1..13} q14{a,b} q{15..22} q{23,24}{a,b} q{25..38} q39{a,b} q{40..99} ss_max{,b})
-failed_queries=(q31 q64 q72)
-
-declare -A configs
-for i in "${queries[@]}"; do
-  configs[${i}]="1 200 1g 1g"
-done
+failed_queries=()
 
 for i in "${queries[@]}"; do
   if [[ " ${failed_queries[@]} " =~ " ${i} " ]]; then
     continue
   fi
-  config="${configs[${i}]}"
-  "${DIR}"/benchmark.sh "${i}" ${config} b
-  "${DIR}"/benchmark.sh "${i}" ${config} g
+  "${DIR}"/benchmark.sh "${i}"
   rm -f ./*.json
 done

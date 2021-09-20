6ec833857d2eb8a036257b2bacd22617de15cee1
Rong Ou <rong.ou@gmail.com>
2021-09-20 09:32:59 -0700

    add iterations

--- benchmark-dgx2.sh
+++ benchmark-dgx2.sh
@@ -6,15 +6,13 @@ DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${DIR}" ]]; then DIR="${PWD}"; fi
 
 queries=(q{1..13} q14{a,b} q{15..22} q{23,24}{a,b} q{25..38} q39{a,b} q{40..99} ss_max{,b})
-failed_queries=()
+failed_queries=(q23{a,b} q37 q82 q95)
 
 for i in "${queries[@]}"; do
   if [[ " ${failed_queries[@]} " =~ " ${i} " ]]; then
     continue
   fi
-  echo "***** Using Arena allocator *****"
-  "${DIR}"/benchmark.sh "${i}" -c 3
-  echo "***** Using Async allocator *****"
-  "${DIR}"/benchmark.sh "${i}" -c 3 -async
+  "${DIR}"/benchmark.sh "${i}" -c 3 -i 10
+  "${DIR}"/benchmark.sh "${i}" -c 3 -async -i 10
   rm -f ./*.json
 done
--- setup.sh
+++ setup.sh
@@ -18,6 +18,7 @@ export MAX_PARTITION_BYTES=1g
 export BATCH_SIZE_BYTES=1g
 export GPU_MEMORY_POOL="ARENA"
 export GPU_DIRECT_RDMA="yes"
+export ITERATIONS=1
 
 while [[ $# -gt 0 ]]; do
   key="$1"
@@ -60,21 +61,29 @@ while [[ $# -gt 0 ]]; do
     export GPU_DIRECT_RDMA="no"
     shift
     ;;
+  -i)
+    export ITERATIONS="$2"
+    shift
+    shift
+    ;;
   *) # unknown option
-    echo "usage: script query# [-g] [-c concurrent_gpu_tasks] [-s shuffle_partitions] [-a] [-t allocation_threshold] [-u]"
+    cat << EOF
+usage: script query# [-g] [-c concurrent_gpu_tasks] [-s shuffle_partitions] [-a] [-t allocation_threshold] [-u] [-async] [-i iterators]
+EOF
     exit 1
     ;;
   esac
 done
 
-#green=$(tput setaf 2)
-#reset=$(tput sgr 0)
-#echo "${green}DATA_DIR=${DATA_DIR}"
-#echo "QUERY=${QUERY}"
-#echo "CONCURRENT_GPU_TASKS=${CONCURRENT_GPU_TASKS}"
+green=$(tput setaf 2)
+reset=$(tput sgr 0)
+echo "${green}DATA_DIR=${DATA_DIR}"
+echo "QUERY=${QUERY}"
+echo "CONCURRENT_GPU_TASKS=${CONCURRENT_GPU_TASKS}"
 #echo "SHUFFLE_PARTITIONS=${SHUFFLE_PARTITIONS}"
 #echo "ALIGNED_IO=${ALIGNED_IO}"
 #echo "ALIGNMENT_THRESHOLD=${ALIGNMENT_THRESHOLD}"
 #echo "UNSPILL=${UNSPILL}"
 #echo "GDS_ENABLED=${GDS_ENABLED}"
-#echo "GDS_HOST_SPILL=${GDS_HOST_SPILL}${reset}"
+#echo "GDS_HOST_SPILL=${GDS_HOST_SPILL}"
+echo "GPU_MEMORY_POOL=${GPU_MEMORY_POOL}${reset}"

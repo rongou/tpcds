8b74cc2294ae4dfbb8fb16886c948f107212c796
Rong Ou <rong.ou@gmail.com>
2021-10-14 09:53:09 -0700

    use 2 dgx2s

--- env-dgx2.sh
+++ env-dgx2.sh
@@ -10,12 +10,14 @@ export SPARK_CUDF_JAR=${JAR_HOME}/cudf.jar
 export SPARK_RAPIDS_PLUGIN_JAR=${JAR_HOME}/rapids-4-spark.jar
 export SPARK_RAPIDS_BENCHMARKS_JAR=${JAR_HOME}/rapids-4-spark-benchmarks.jar
 
-export SPARK_MASTER_HOST=127.0.0.1
-export SPARK_LOCAL_IP=${SPARK_MASTER_HOST}
+#export SPARK_MASTER_HOST=127.0.0.1
+#export SPARK_LOCAL_IP=${SPARK_MASTER_HOST}
+export SPARK_MASTER_HOST=10.150.30.3
+export SPARK_LOCAL_IP=$(ifconfig enp134s0f0 | grep 'inet ' | cut -d' ' -f10)
 
-export SPARK_EXECUTOR_INSTANCES=16
-export SPARK_EXECUTOR_CORES=3
-export SPARK_TASK_RESOURCE_GPU_AMOUNT=0.3333
+export SPARK_EXECUTOR_INSTANCES=32
+export SPARK_EXECUTOR_CORES=6
+export SPARK_TASK_RESOURCE_GPU_AMOUNT=0.1666
 
 # Hadoop
 export HADOOP_HOME=${HOME}/hadoop
@@ -25,5 +27,4 @@ export LD_LIBRARY_PATH=${HADOOP_HOME}/lib/native${LD_LIBRARY_PATH:+:${LD_LIBRARY
 export NVTX_ENABLED=false
 
 # Data
-export DATA_DIR="/raid2/spark-team/tpcds/sf5000-parquet/useDecimal=false,useDate=true,filterNull=false"
-#export DATA_DIR="/raid/spark-team/tpcds/sf3000-parquet/useDecimal=false,useDate=true,filterNull=false"
+export DATA_DIR="/raid/spark-team/tpcds/sf5000-parquet/useDecimal=false,useDate=true,filterNull=false"

f54686de948d77213477b66af5dd67f06a206edb
Rong Ou <rong.ou@gmail.com>
2021-10-19 11:16:00 -0700

    only start master on master host

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-"${SPARK_HOME}"/sbin/start-master.sh
+if [[ "${SPARK_MASTER_HOST}" == "${SPARK_LOCAL_IP}" ]]; then
+  "${SPARK_HOME}"/sbin/start-master.sh
+fi
 "${SPARK_HOME}"/sbin/start-worker.sh spark://"${SPARK_MASTER_HOST}":7077
